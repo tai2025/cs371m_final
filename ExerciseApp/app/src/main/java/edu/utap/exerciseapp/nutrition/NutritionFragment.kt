49514cb018073f79fe6cd553f21150cf7b20ba84
aristotai <144725248+aristotai@users.noreply.github.com>
2024-04-18 23:14:15 -0500

    still trying to work

---
+++ ExerciseApp/app/src/main/java/edu/utap/exerciseapp/nutrition/NutritionFragment.kt
@@ -0,0 +1,34 @@
+package edu.utap.exerciseapp.nutrition
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import  edu.utap.exerciseapp.databinding.NutritionPageBinding
+class NutritionFragment: Fragment() {
+    private var _binding: NutritionPageBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel : MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = NutritionPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
